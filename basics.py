a77b9e9d2a4a7cf4866bb969d578d80d1d8ee530
IngarsPoliters <politersingars2@gmail.com>
2021-02-05 20:04:41 +0000

    Looked at the basics.

---
+++ basics.py
@@ -0,0 +1,23 @@
+#Ingars Politers
+#Python basics
+#2021-02-05
+
+print("Hello, world!")
+
+a = 1
+b = 1.0
+s = "'Hello', world from a string!"
+t = '"Hello", from a different string'
+
+print(a, b, s, t)
+
+print(type(a))
+
+print(type(b))
+
+print(s[1])
+
+print(s[2:10])
+
+print(s[5:10:2])
+
