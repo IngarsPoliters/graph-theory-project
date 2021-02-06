2928d8075afa5faabc16cdd25e88606289b18035
IngarsPoliters <politersingars2@gmail.com>
2021-02-06 18:32:38 +0000

    List and dictionaries

--- basics.py
+++ basics.py
@@ -21,3 +21,33 @@ print(s[2:10])
 
 print(s[5:10:2])
 
+x = [1, 2, 3, "Hello", 1.0]
+print(x)
+print(x[0])
+print(x[2])
+print(x[-1])
+
+print("_______________________")
+
+for i in x[::2]:
+    print(i)
+    print(i + i)
+
+for i in range(100):
+    print(i)
+    
+d = {"no_wheels": 4, "make": "Skoda"}
+
+print(d["make"])
+
+d["model"] = "Superb"
+
+print(d["model"])
+
+r = [1,2,3,4]
+
+print(r)
+
+s = [i*i for i in r]
+
+print(s)
